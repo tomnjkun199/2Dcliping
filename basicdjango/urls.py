d7adc44074720adc888606e3b5d204b4049621c3
Suppachai Booncharoen <ztonkung@gmail.com>
2021-02-01 22:10:28 +0700

    eighth

--- basicdjango/urls.py
+++ basicdjango/urls.py
@@ -23,8 +23,5 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.hello),
-    path('calculate',views.cal),
-
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root':      settings.STATIC_ROOT}) 
+    path('calculate',views.cal)
 ]
