0c714ae29caab826f8d06153bb1b553d6479bb7a
Suppachai Booncharoen <ztonkung@gmail.com>
2021-02-01 21:09:21 +0700

    seventh

--- basicdjango/settings.py
+++ basicdjango/settings.py
@@ -118,4 +118,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = [BASE_DIR, 'staticfiles']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
